9a85fbd64acf3ca3039ac68f50a980f24a5d7f8f
mineng <s.minhaeng@gmail.com>
2019-03-09 15:56:39 +0900

    2Dリストのコマンド

---
+++ 2D_list.py
@@ -0,0 +1,11 @@
+users = [["Bob", 11],
+         ["Tom", 34],
+         ["Ken", 65]]
+
+# print(f" {users}さんは{users}歳です")
+
+print(f" {users[0][0]}さんは{users[0][1]}歳です")
+
+# Tomさんは34歳です
+
+print(f" {users[1][0]}さんは{users[1][1]}歳です")
