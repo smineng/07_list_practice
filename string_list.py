8eb8b568a3eb707ebff033e559ed4807125b024d
mineng <s.minhaeng@gmail.com>
2019-03-09 16:09:10 +0900

    forの使い方

--- string_list.py
+++ string_list.py
@@ -1,6 +1,10 @@
 members = ["Bob", "Tom", "Ken"]
 
+print(members[0])
+print(members[1])
 print(members[2])
 
-print(members[1])
-print(members[0])
+# 1分課題:　上記の3行分のコードをforを使って書き直せ
+
+for member in members:
+    print(member)
